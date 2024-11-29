e271de97b6fe3efb970dbc5fff5194d7c302e9b8
Michał Wójcik <wojcikmichal98@gmail.com>
2024-11-29 12:41:12 +0100

    Add example ROS code using aether library

---
+++ ros_ws/src/aether_app/src/aether_test.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+#include <aether/mapped_localization.hpp>
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    printf("hello world aether_app package\n");
+    return 0;
+}
